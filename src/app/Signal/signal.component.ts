2ca3e810a1aa2ac4a414f7a7e02bb702d99a9a6f
MohamedSahir <mailtosahirm@gmail.com>
2023-09-21 14:11:57 +0000

    signal currency added

--- src/app/Signal/signal.component.ts
+++ src/app/Signal/signal.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {TransitionNameDirective} from "../directives/transition-name.directive";
 import {RouterLink} from "@angular/router";
-
+import { Component, computed, effect, signal } from '@angular/core';
 @Component({
   selector: 'app-signal',
   standalone: true,
@@ -12,9 +12,35 @@ import {RouterLink} from "@angular/router";
 })
 export class SignalComponent {
 
+ // `signal`: create a settable signal
+ readonly name = signal('World');
+ // `computed`: create a signal of computed value (readonly)
+ readonly message = computed(() => {
+   return `Hello ${this.name()}!`;
+ });
 
+ selectedVehicle = signal<Vehicle>({ id: 1, name: 'AT-AT', price: 10000 });
+
+ constructor() {
+   effect(() => {
+     console.log(this.message());
+   });
+ }
+
+
+
+ currency = signal('USD');
+
+
+// sets new value to currency
   test(){
-    
+    this.currency.set('CAD');
   }
 
 }
+
+export interface Vehicle {
+  id: number;
+  name: string;
+  price: number;
+ }
